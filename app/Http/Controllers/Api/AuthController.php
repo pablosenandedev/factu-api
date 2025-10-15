12672a8c9869d74ab78891969af19607d30a3740
Pablo <pablosenandedev@gmail.com>
2025-10-15 13:30:20 +0200

    Update auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,29 +3,85 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class AuthController extends Controller
 {
     //
-    function login()
+    function login(Request $request)
     {
-        return response()->json(['message' => 'Login successful']);
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful.',
+            'token' => $token,
+            'data' => $user,
+        ], 200);
     }
 
-    function register()
+    function register(Request $request)
     {
-        return response()->json(['message' => 'Registration successful']);
+        $validator = FacadesValidator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User registered successfully.',
+            'token' => $token,
+            'data' => $user,
+        ], 201);
     }
 
-    function logout()
+    public function logout(Request $request)
     {
-        return response()->json(['message' => 'Logout successful']);
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful.',
+        ], 200);
     }
 
-    function me()
+    public function me(Request $request)
     {
-        return response()->json(['user' => 'User details']);
+
+        $user = $request->user()
+            ->load('companies');
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User profile fetched successfully.',
+            'data' => $user,
+        ], 200);
     }
 
 }
